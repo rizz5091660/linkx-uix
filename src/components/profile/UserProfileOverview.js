b9f1166df5980846184dfdea7ec6ed585bb3f5fe
Rizky <rizky.akbar.sani@gmail.com>
2019-03-19 18:01:37 +0700

    Feed

--- src/components/profile/UserProfileOverview.js
+++ src/components/profile/UserProfileOverview.js
@@ -5,97 +5,87 @@ import instagram from "../../images/icon/instagram.png";
 import youtube from "../../images/icon/youtube.png";
 import shop from "../../images/icon/online-store.png"
 import signal from "../../images/icon/signal.gif"
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Row,
-  Col
-} from "shards-react";
+import {Card,CardHeader,CardBody,Row,Col} from "shards-react";
 import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 
-const UserProfileOverview = ({ userDetails }) => (
-  <Card small className="mb-4 pt-3">
-    <CardHeader className="border-bottom text-center">
-      <NavLink to="/profile">
-        <div className="mb-3 mx-auto">
-          <img
-            className="rounded-circle"
-            src={userDetails.avatar}
-            alt={userDetails.name}
-            width="110"
-          />
-        </div>
-        <h4 className="mb-0">{userDetails.name}</h4>
-      </NavLink>
-    </CardHeader>
-    <CardBody>
-      <Row>
-        <Col className="ml-4" lg="12">
-          <div style={{ display: "inline" }}><a href="https://www.youtube.com/user/natefiggs"><img src={youtube} width="30" /></a> </div>
-          <div style={{ display: "inline" }}><span className="text-muted  mb-2">{userDetails.jobTitle}</span></div>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="ml-4" lg="12">
-          <div style={{ display: "inline" }}><a href="https://www.instagram.com/natefiggs/"><img src={instagram} width="30" /></a> </div>
-          <div style={{ display: "inline" }}><span className="text-muted  mb-2">{userDetails.jobTitle2}</span></div>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="ml-4" lg="12">
-          <div style={{ display: "inline" }}><a href="https://www.facebook.com/nathan.figueroa.12"><img src={facebook} width="30" /></a> </div>
-          <div style={{ display: "inline" }}><span className="text-muted  mb-2">{userDetails.jobTitle3}</span></div>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="ml-4" lg="12">
-          <div style={{ display: "inline" }}><a href="#"><img src={shop} width="30" /></a> </div>
-          <div style={{ display: "inline" }}><span className="text-muted  mb-2">My Shop</span></div>
-        </Col>
-      </Row>
-      <div className="border-bottom p-2 mb-2"></div>
-      <Row>
-        <Col lg="12">
-          <span>Who's viewed your profile</span><strong className="text-muted  mb-2"> 28 </strong>
-        </Col>
-        <Col lg="12">
-          <span>Who's requested for collaboration</span><strong className="text-muted  mb-2"> 5 </strong> <img src={signal} width="30" />
-        </Col>
-        <Col lg="12">
-          <span>Who's viewed of your post</span><strong className="text-muted mb-2"> 124 </strong>
-        </Col>
-      </Row>
-      <div className="border-bottom p-2 mb-2"></div>
-      <Row>
-        <Col lg="12">
-          <span>{userDetails.metaValue}</span>
-        </Col>
-      </Row>
-    </CardBody>
-  </Card>
-);
-
-UserProfileOverview.propTypes = {
-  /**
-   * The user details object.
-   */
-  userDetails: PropTypes.object
-};
-
-UserProfileOverview.defaultProps = {
-  userDetails: {
-    name: "Nathan Figueroa",
-    avatar: require("./../../images/avatars/nathanfigueroa.jpg"),
-    jobTitle: "421,420 followers",
-    jobTitle2: "1,011 followers",
-    jobTitle3: "3,656 followers",
-    performanceReportTitle: "Workload",
-    performanceReportValue: 74,
-    metaTitle: "Description",
-    metaValue:
-      "Looking forward for food projects!!🍕🍔🌯 "
+class UserProfileOverview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      userDetails: {
+        jobTitle: "421,420 followers",
+        jobTitle2: "1,011 followers",
+        jobTitle3: "3,656 followers",
+        performanceReportTitle: "Workload",
+        performanceReportValue: 74,
+        metaTitle: "Description",
+        metaValue:
+          "Looking forward for food projects!!🍕🍔🌯 "
+      },
+      profile:{
+        accSocMedStats:[{socMed:{}}],
+        accLinkxStats:{}
+      }
+    }
   }
-};
+  componentDidMount() {
+    fetch("http://localhost:8080/api/account/398765f0-4220-11e9-8972-aca63e449b3c/summary").then((Response) => Response.json())
+    .then((findresponse) => {this.setState({ profile: findresponse})})
+  }
+  render(){
+    const {
+      userDetails,
+      profile
+    }= this.state;
+    return(
+      <Card small className="mb-4 pt-3">
+      <CardHeader className="border-bottom text-center">
+        <NavLink to="/profile">
+          <div className="mb-3 mx-auto">
+            <img
+              className="rounded-circle"
+              src={profile.avatar}
+              alt={profile.name}
+              width="110"
+            />
+          </div>
+          <h4 className="mb-0">{profile.name}</h4>
+          <span>{profile.position} at {profile.organization}</span>
+        </NavLink>
+      </CardHeader>
+      <CardBody>
+      {profile.accSocMedStats.map((asm) => (
+        <Row>
+        <Col className="ml-4" lg="12">
+          <div style={{ display: "inline" }}><a href="https://www.youtube.com/user/natefiggs"><img src={asm.socMed.avatarFullPath} width="30" /></a> </div>
+          <div style={{ display: "inline" }}><span className="text-muted  mb-2">{asm.totalFollower} followers</span></div>
+        </Col>
+        </Row>
+      ))}
+        <Row>
+          <Col className="ml-4" lg="12">
+            <div style={{ display: "inline" }}><a href="#"><img src={shop} width="30" /></a> </div>
+            <div style={{ display: "inline" }}><span className="text-muted  mb-2">My Shop</span></div>
+          </Col>
+        </Row>
+        <div className="border-bottom p-2 mb-2"></div>
+        <Row>
+        <Col lg="12"> <span>Who's requested for collaboration</span><strong className="text-muted  mb-2"> </strong> {profile.accLinkxStats.nReqCollab} <img src={signal} width="30" /></Col>
+          <Col lg="12"><span>Who's viewed your profile</span><strong className="text-muted  mb-2"> {profile.accLinkxStats.nViewProfile} </strong> </Col>
+          <Col lg="12"><span>Who's viewed of your post</span><strong className="text-muted mb-2"> {profile.accLinkxStats.nViewPost}  </strong></Col>
+        </Row>
+        <div className="border-bottom p-2 mb-2"></div>
+        <Row>
+          <Col lg="12">
+            <span>{profile.description}</span>
+          </Col>
+        </Row>
+      </CardBody>
+    </Card>
+    );
+
+
+  }
+}
 
 export default UserProfileOverview;
--- src/views/Feed.js
+++ src/views/Feed.js
@@ -16,105 +16,100 @@ import {
 class Feed extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      feeds:[]
+    this.state = {
+      feeds: []
     }
 
   }
 
   componentDidMount() {
-    fetch("http://www.mocky.io/v2/5c79ce794900007000a5a651").then((Response) => Response.json()).then((findresponse)=> {
-      this.setState({
-              feeds: findresponse
-            })
-        }
-    )
-}
+    fetch("http://www.mocky.io/v2/5c79ce794900007000a5a651").then((Response) => Response.json())
+    .then((findresponse) => {this.setState({ feeds: findresponse})})
+  }
 
   render() {
     const {
-      PostsListOne,
       feeds
-    }= this.state;
-    return(
+    } = this.state;
+    return (
       <Container fluid className="main-content-container px-4">
-          <Row noGutters className="page-header py-4"/>
-          <div className="centerPosition">
+        <Row noGutters className="page-header py-4" />
+        <div className="centerPosition">
           <Row>
             <Col lg="3">
               <UserProfileOverview />
             </Col>
             <Col lg="5">
-            <Row>
-              <Col lg="12" sm="12" >
-              <Card small className="card-post mb-4">
-                <CardBody>
-                  <h5 className="card-title">
-                  Start a post
+              <Row>
+                <Col lg="12" sm="12" >
+                  <Card small className="card-post mb-4">
+                    <CardBody>
+                      <h5 className="card-title">
+                        Start a post
                   </h5>
-                  <p className="card-text text-muted">
-                  
-                  </p>
-                </CardBody>
-                <CardFooter className="border-top d-flex" style={{backgroundColor:"#f3f6f8"}}>
-                <span className="card-post__author-name">
-                       <b style={{color:"#0073b1"}}>Share a project</b> on LinkedIn
-                      </span> 
-                </CardFooter>
-              </Card>
-              </Col>
-            </Row>
-           
+                      <p className="card-text text-muted">
+
+                      </p>
+                    </CardBody>
+                    <CardFooter className="border-top d-flex" style={{ backgroundColor: "#f3f6f8" }}>
+                      <span className="card-post__author-name">
+                        <b style={{ color: "#0073b1" }}>Share a project</b> on LinkedIn
+                      </span>
+                    </CardFooter>
+                  </Card>
+                </Col>
+              </Row>
 
 
-          {feeds.map((post, idx) => (
-             <Row>
-            <Col lg="12" md="12" sm="12" className="mb-4" key={idx}>
-              <Card small className="card-post card-post--1">
-                <div style={{padding:"1rem"}} >
-                   <b><a href="#" className="text-fiord-blue">{post.likeBy}  </a></b> likes this
+
+              {feeds.map((post, idx) => (
+                <Row>
+                  <Col lg="12" md="12" sm="12" className="mb-4" key={idx}>
+                    <Card small className="card-post card-post--1">
+                      <div style={{ padding: "1rem" }} >
+                        <b><a href="#" className="text-fiord-blue">{post.likeBy}  </a></b> likes this
                 </div>
-                <div
-                  className="card-post__image"
-                  style={{ backgroundImage: `url('${post.backgroundImage}')` }}
-                >
-                  <Badge
-                    pill
-                    className={`card-post__category bg-${post.categoryTheme}`}
-                  >
-                    {post.category}
-                  </Badge>
-                  <div className="card-post__author d-flex">
-                    <a
-                      href="#"
-                      className="card-post__author-avatar card-post__author-avatar--small"
-                      style={{ backgroundImage: `url('${post.authorAvatar}')` }}
-                    >
-                      Written by {post.author}
-                    </a>
-                  </div>
-                </div>
-                <CardBody>
-                  <h5 className="card-title">
-                    <a href="#" className="text-fiord-blue">
-                      {post.title}
-                    </a>
-                  </h5>
-                  <p className="card-text d-inline-block mb-3">{post.body}</p>
-                  <span className="text-muted">{post.date}</span>
-                </CardBody>
-              </Card>
-            </Col>
-            </Row>
-          ))}
+                      <div
+                        className="card-post__image"
+                        style={{ backgroundImage: `url('${post.backgroundImage}')` }}
+                      >
+                        <Badge
+                          pill
+                          className={`card-post__category bg-${post.categoryTheme}`}
+                        >
+                          {post.category}
+                        </Badge>
+                        <div className="card-post__author d-flex">
+                          <a
+                            href="#"
+                            className="card-post__author-avatar card-post__author-avatar--small"
+                            style={{ backgroundImage: `url('${post.authorAvatar}')` }}
+                          >
+                            Written by {post.author}
+                          </a>
+                        </div>
+                      </div>
+                      <CardBody>
+                        <h5 className="card-title">
+                          <a href="#" className="text-fiord-blue">
+                            {post.title}
+                          </a>
+                        </h5>
+                        <p className="card-text d-inline-block mb-3">{post.body}</p>
+                        <span className="text-muted">{post.date}</span>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                </Row>
+              ))}
             </Col>
             <Col lg="4">
-              <Follow type="feed"/>
+              <Follow type="feed" />
             </Col>
           </Row>
-          </div>
-        </Container>
-          );
+        </div>
+      </Container>
+    );
   }
 
 }
